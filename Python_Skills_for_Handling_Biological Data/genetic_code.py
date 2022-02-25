bc2d98733bec40e980d93884ffed1ca660a4b11a
DrPierreChang <aaa.lavrentev@gmail.com>
2022-02-25 19:53:18 +1000

    dict with amino acids

---
+++ Python_Skills_for_Handling_Biological Data/genetic_code.py
@@ -0,0 +1,9 @@
+genetic_code = {
+    "UUU": "F",  # phenilalanyne
+    "UCU": "S",  # serine
+    "UAU": "Y",  # tyrosine
+    "UGU": "C",  # cysteine
+    "UUC": "F",  # phenilalanyne
+    "UCC": "S",  # serine
+    "UAC": "Y",  # tyrosine etc.
+}
